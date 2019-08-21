3d2c81bc3a133b7c3fa135280897f8215e9f9aa3
Gustavo Carvalho Dos Santos <47918607805@info.local>
2019-08-21 17:31:16 -0300

    up

--- Sprint-2-BackEnd/Senai.Filmes.WebApi/Senai.Filmes.WebApi/Controllers/GenerosController.cs
+++ Sprint-2-BackEnd/Senai.Filmes.WebApi/Senai.Filmes.WebApi/Controllers/GenerosController.cs
@@ -12,6 +12,6 @@ namespace Senai.Filmes.WebApi.Controllers
     [ApiController]
     public class GenerosController : ControllerBase
     {
-        List
+        
     }
 }
--- Sprint-2-BackEnd/Senai.Filmes.WebApi/Senai.Filmes.WebApi/Repository/GeneroRepository.cs
+++ Sprint-2-BackEnd/Senai.Filmes.WebApi/Senai.Filmes.WebApi/Repository/GeneroRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Senai.Filmes.WebApi.Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,14 @@ namespace Senai.Filmes.WebApi.Repository
 {
     public class GeneroRepository
     {
+        // aonde que será feita essa comunicação
+        // private string StringConexao = "Data Source=.\\SqlExpress;Initial Catalog=T_SStop;User Id=sa;Pwd=132;";
+        private string StringConexao = "Data Source=localhost;Initial Catalog=RoteiroFilmes;Integrated Security=true;";
+        public List<GeneroDomain> ListarTodos()
+        {
+           List<GeneroDomain> generos = new List<GeneroDomain>();
+            return generos;
+        }
+
     }
 }
